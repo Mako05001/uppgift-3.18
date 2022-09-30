5ae286f81e7047b25f350c9662a8f754cc2a95c6
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-30 14:04:12 +0200

    Add project files.

---
+++ uppgift 3.18a/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace uppdrag3._14
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Skriv ditt förenamn och efternamn");
+            string Na = Console.ReadLine();
+            int NamnIndex = Na.IndexOf(' ');
+            string förenamn = Na[..NamnIndex];
+            string efternamn = Na[(NamnIndex + 1)..];
+            if (förenamn.CompareTo(efternamn) < 0)
+            {
+                Console.WriteLine("Ditt förenamn kommer före ditt efternamn i bokstavsordning");
+            }
+            else if (förenamn.CompareTo(efternamn) > 0)
+            {
+                Console.WriteLine("Ditt efternamn kommer före ditt förenamn i bokstavsordning");
+            }
+            else
+            {
+                Console.WriteLine("Ditt För och efternamn kommer på samma plat i bokstavsordning");
+            }
+
+        }
+    }
+}
